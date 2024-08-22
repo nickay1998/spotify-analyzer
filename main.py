81a0450f83a607acfa3f5f32927f0ae0707f6a89
Nicolas Au-Yeung <nickay1998@gmail.com>
2024-08-22 18:57:19 +0700

    feat: add SearchResults class to contain search results and add load more button for more results

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import streamlit as st
-from spotify import check_spotify_access, search, process_artist_data
+from spotify import check_spotify_access, search
 from streamlit_js_eval import streamlit_js_eval
 
 if __name__ == '__main__':
 
     # Set title in tab
     st.set_page_config(page_title="Spotify Analysis", layout="wide")
-
+    
     # Fetch initial window width
     streamlit_js_eval(js_expressions='window.innerWidth', key="window_width")
-    
+
     check_spotify_access()
 
     with st.form(key="Search"):
@@ -18,5 +18,8 @@ if __name__ == '__main__':
 
     if submitted:
         search_results = search(search_text)
-        
-        process_artist_data(search_results["artists"])
+
+        if "search_container" not in st.session_state:
+            st.session_state["search_container"] = st.empty()
+
+        search_results.display_results("artists")
--- spotify/search.py
+++ spotify/search.py
@@ -1,8 +1,47 @@
-from utils import get_data
+import streamlit as st
+from utils import get_data, display_cards
+from .artist import process_artist_data
 
-def search(search_term):
-    search_term = search_term.replace(" ", "+")
-    url = f"https://api.spotify.com/v1/search?q={search_term}&type=album%2Cartist%2Ctrack"
+def search(search_text):
+    if "search_results" not in st.session_state:
+        st.session_state["search_results"] = {}
+    elif search_text in st.session_state["search_results"].keys():
+        return st.session_state["search_results"][search_text]
+
+    search_text = search_text.replace(" ", "+")
+    url = f"https://api.spotify.com/v1/search?q={search_text}&type=album%2Cartist%2Ctrack"
     search_json = get_data(url)
 
-    return search_json
+    if search_text not in st.session_state["search_results"].keys():
+        st.session_state["search_results"][search_text] = SearchResults(search_json)
+
+    return st.session_state["search_results"][search_text]
+
+class SearchResults:
+    def __init__(self, data):
+        self.artist_json = {data["artists"]["offset"]: data["artists"]}
+        self.artist_next = data["artists"]["next"]
+        self.artists = process_artist_data(data["artists"])
+
+    def add_artists(self):
+        data = get_data(self.artist_next)
+        self.artist_json[data["artists"]["offset"]] = data["artists"]
+        self.artist_next = data["artists"]["next"]
+        self.artists = self.artists + process_artist_data(data["artists"])
+        self.display_results("artists", data["artists"]["next"] != None)
+
+    def display_results(self, type, button = True):
+        with st.session_state["search_container"].container():
+            self.display_cards(type)
+            
+            if button:
+                self.load_more_button(type)
+
+    def display_cards(self, type):
+        if type == "artists":
+            st.write(f"Showing {len(self.artists)} of {self.artist_json[0]['total']} {type}")
+            display_cards(self.artists)
+    
+    def load_more_button(self, type):
+        if type == "artists":
+            st.button(f"Load more {type}...", on_click=self.add_artists)
